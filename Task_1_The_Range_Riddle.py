7a74cd574bc6bdbb58d2a4dbbe14c39190c39e1f
nikitaan1 <ancevskinikita@gmail.com>
2024-03-21 10:58:56 -0400

    Changes to task 1

--- Task_1_The_Range_Riddle.py
+++ Task_1_The_Range_Riddle.py
@@ -3,6 +3,7 @@ import random
 days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 moods = ["Happy", "Sad", "Energetic", "Calm"]
 
-for days in days_of_week:
+for i in range(len(days_of_week)):
+    days = days_of_week[i]
     mood = random.choice(moods)
     print(f"On {days} you were feeling {mood}")
